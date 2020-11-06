f62802c5f3eb940f92c92f5c725abd8e24423a3b
pheno-cat <catherinejalomo@gmail.com>
2020-11-06 13:59:25 -0600

    cj commit 3_reorg files

---
+++ script.js
@@ -0,0 +1,18 @@
+var btn = $("#btn");
+var text = $("#text");
+
+//ajax call
+
+function search() {
+  //clears the container if something was there before
+  $("#textContainer").empty();
+  console.log(text.val());
+  $.ajax({
+    url: "" + text.val(),
+    method: "GET",
+  }).then(function (data) {
+    console.log(data);
+  });
+}
+
+btn.on("click", search);
