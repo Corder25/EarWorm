d8b632b0f7e99e18cf858949b2a947b37a676f24
pheno-cat <catherinejalomo@gmail.com>
2020-11-09 17:38:59 -0600

    image resizing

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// var btn = $("#btn");
-// var text = $("#text");
-
-// //ajax call
-
-// function search() {
-//   //clears the container if something was there before
-//   $("#textContainer").empty();
-//   console.log(text.val());
-//   $.ajax({
-//     url: "" + text.val(),
-//     method: "GET",
-//   }).then(function (data) {
-//     console.log(data);
-//   });
-// }
-
-// btn.on("click", search);
+$(document).ready(function () {
+  $(".carousel").carousel();
+});
