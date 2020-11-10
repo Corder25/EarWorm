bcaafe4dc7df3ac9f7a7b90c959c899ecec0dc8e
pheno-cat <catherinejalomo@gmail.com>
2020-11-09 20:52:32 -0600

    apple api

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-$(document).ready(function () {
-  $(".carousel").carousel();
-});
+var containerMain = document.querySelector(".container-main");
+var artistImage = document.querySelector(".artist");
+var media = document.querySelector(".media");
+var overlay = document.querySelector(".overlay");
+var searchElem = document.querySelector(".input-field");
+
+var getContent = (search) => {
+  var url = new URL("https://itunes.apple.com/search");
+  var params = { term: search, media: "musicVideo" };
+  url.search = new URLSearchParams(params);
+  fetch(url, { method: "POST" })
+    .then((results) => results.json())
+    .then((data) => {});
+};
+
+getContent("radiohead");
