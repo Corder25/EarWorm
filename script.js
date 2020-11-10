7ec9a510e82cbda67b9d83b79739ce7c8ab43261
pheno-cat <catherinejalomo@gmail.com>
2020-11-09 22:19:02 -0600

    jqeury apple api addition

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ var media = document.querySelector(".media");
 var overlay = document.querySelector(".overlay");
 var searchElem = document.querySelector(".input-field");
 
+searchElem.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    getContent(searchElem.value);
+  }
+});
+
 var getContent = (search) => {
   var url = new URL("https://itunes.apple.com/search");
   var params = { term: search, media: "musicVideo" };
@@ -11,6 +17,14 @@ var getContent = (search) => {
   fetch(url, { method: "POST" })
     .then((results) => results.json())
     .then((data) => {});
+  var resultsHTML = data.results
+    .map(
+      (result) => `
+        <div style="background-image: url(${result.artworkUrl100});"
+        onclick="openMedia('${result.previewUrl}', '${result.trackCensoredName}')" class="result"></div>
+        `
+    )
+    .join("");
+  containerMain.innerHTML = resultsHTML;
 };
-
-getContent("radiohead");
+getContent();
