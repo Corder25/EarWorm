637ca4e40a038552e41e56769c5b3f7f92fa4b9b
pheno-cat <catherinejalomo@gmail.com>
2020-11-13 13:37:52 -0600

    commit changes

--- script.js
+++
@@ -1,7 +0,0 @@
-var splash = document.querySelector(".splash");
-
-document.addEventListener("DOMContentLoaded", (e) => {
-  setTimeout(() => {
-    splash.classList.add("display-none");
-  }, 2000);
-});
